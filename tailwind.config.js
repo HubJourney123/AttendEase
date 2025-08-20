28ba62610b0742f9a04a63dab1a1cda3c85d90c5
HubJourney123 <shahriar@ese.kuet.ac.bd>
2025-08-20 20:23:26 +0600

    Alhamdulillah

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class", // allows manual dark mode toggle
+  darkMode: "media", // allows manual dark mode toggle
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
